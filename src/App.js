cd60e9cddc2e1db169f5a08646dfbe428582498d
pdumpster <Mateusz.S-ki@proton.me>
2024-12-10 20:33:11 +0100

    Analyz

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Task2 from "./components/Task2";
 import Task3 from "./components/Task3";
 import Task4 from "./components/Task4";
 import Task5 from "./components/Task5";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Router>
       <div className="container">
+        <Analytics />
         <h1 className="my-4">React</h1>
         <nav>
           <ul className="nav nav-pills">
